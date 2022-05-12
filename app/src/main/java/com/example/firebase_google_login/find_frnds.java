4495174562dfa51dfebc1af4b287cacbbf272029
NIKHIL KUMAR <76584908+nikhil78031@users.noreply.github.com>
2022-05-12 23:24:39 +0530

    friends update

---
+++ app/src/main/java/com/example/firebase_google_login/find_frnds.java
@@ -0,0 +1,88 @@
+package com.example.firebase_google_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SearchEvent;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class find_frnds extends AppCompatActivity {
+    RecyclerView recview;
+    DatabaseReference mRef;
+    myadapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_frnds);
+        mRef = FirebaseDatabase.getInstance().getReference().child("User");
+        recview = (RecyclerView) findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options = new FirebaseRecyclerOptions.Builder<model>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("User"),model.class)
+                .build();
+
+        adapter = new myadapter(options);
+        recview.setAdapter(adapter );
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.searchmenu,menu);
+
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                processsearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                processsearch(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    private void processsearch(String s) {
+        FirebaseRecyclerOptions<model> options = new FirebaseRecyclerOptions.Builder<model>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("User").orderByChild("email").startAt(s).endAt(s+"\uf8ff"),model.class)
+                .build();
+        Log.i("DATA",mRef.getKey());
+        adapter = new myadapter(options);
+        adapter.startListening();
+        recview.setAdapter(adapter);
+    }
+}
