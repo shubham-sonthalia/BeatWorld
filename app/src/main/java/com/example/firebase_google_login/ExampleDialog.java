8b4c274740b9a576b074f12691c74b04f9c73338
avinash18227 <avinash18227@iiitd.ac.in>
2022-05-13 11:30:34 +0530

    10th commit on this branch

--- app/src/main/java/com/example/firebase_google_login/ExampleDialog.java
+++ app/src/main/java/com/example/firebase_google_login/ExampleDialog.java
@@ -5,14 +5,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
@@ -22,13 +20,6 @@ public class ExampleDialog extends AppCompatDialogFragment {
     private EditText editTextPassword;
     Button image_upload,music_upload;
     private ExampleDialogListener listener;
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Uri uri = data.getData();
-
-    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -49,8 +40,6 @@ public class ExampleDialog extends AppCompatDialogFragment {
                         .start();
             }
         });
-
-
 
         music_upload.setOnClickListener(new View.OnClickListener() {
             @Override
